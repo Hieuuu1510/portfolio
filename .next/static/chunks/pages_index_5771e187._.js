d3c1e8f327b4fa67a878b6151676303af4af1867
Hieu <hieuhienlanh03@gmail.com>
2025-03-16 00:35:09 +0700

    chore: udpate contect text home screen

--- .next/static/chunks/pages_index_5771e187._.js
+++ .next/static/chunks/pages_index_5771e187._.js
@@ -9,10 +9,10 @@
     "static/chunks/node_modules_next_dist_shared_lib_f58cf1f3._.js",
     "static/chunks/node_modules_next_dist_client_ad5f6842._.js",
     "static/chunks/node_modules_next_dist_ad5d9790._.js",
-    "static/chunks/node_modules_next_251e4b30._.js",
+    "static/chunks/node_modules_next_image_f00481cb.js",
     "static/chunks/node_modules_react-dom_82bb97c6._.js",
     "static/chunks/node_modules_1b7400a8._.js",
-    "static/chunks/[root of the server]__a43c9200._.js"
+    "static/chunks/[root of the server]__1f48ead1._.js"
   ],
   "source": "entry"
 });
--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,8 +1,26 @@
-import NavBar from "@/libs/src/navbar";
 import React from "react";
-
+import Image from "next/image";
 const Home = () => {
-  return <>Home he</>;
+  return (
+    <div className="home_page">
+      <div className="home_page_content">
+        <div className="home_page_content_title">
+          <div>Hello World, I'm</div>
+          <h1>Hiếu bão tố</h1>
+          <div className="text_animation">Frontend Developer</div>
+          <div className="welcome_text">Welcome to My personal website. 🫰</div>
+        </div>
+        <div className="home_page_image">
+          <Image
+            src="/images/home_page_image.png"
+            alt="home_page_image"
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
